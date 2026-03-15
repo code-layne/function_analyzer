3c4a7eb18603970867984a809f5970e634c8e179
Layne Shepherd <layne_s@icloud.com>
2026-03-15 17:09:11 -0400

    added quadratic example

--- src/main.rs
+++ src/main.rs
@@ -13,4 +13,16 @@ fn main() {
     println!("Evaluate function for x=3: f(3) = {}", linear_fx.evaluate(3.0));
     println!("Fx y-intercept: f(0) = {}", linear_fx.y_intercept());
     println!("First derivative: f'(x) = {}", linear_fx.derivative().format_pretty());
+
+    let quadratic_coefficients = vec![1.0, 3.0, 2.0];
+    let quadratic_fx = Polynomial::new(quadratic_coefficients);
+
+    println!("f(x) = {}", quadratic_fx.format_pretty());
+    println!("Function degree: {}", quadratic_fx.degree());
+    println!("Leading coefficient: {}", quadratic_fx.leading_coefficient());
+
+    println!("Evaluate function for x=3: f(3) = {}", quadratic_fx.evaluate(3.0));
+    println!("Fx y-intercept: f(0) = {}", quadratic_fx.y_intercept());
+    println!("First derivative: f'(x) = {}", quadratic_fx.derivative().format_pretty());
+
 }
